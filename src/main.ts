109e3772325147823eb66f71a857d79511689da1
Brock Chelle <brocklchelle@gmail.com>
2022-09-12 14:19:48 -0600

    #15: Create function to generate the result of all game outcomes

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { OddsData } from './@types/oddsData'
 import { OfpData } from './@types/ofpData'
 import TEST_ODDS_DATA_JSON from './data/testOddsData.json'
 import TEST_OFP_DATA_JSON from './data/testOfficeFootballPoolData.json'
-import { mergeOfpAndOddsData } from './utils/game'
+import { generateOutcomes, mergeOfpAndOddsData } from './utils/game'
 
 const TEST_OFP_DATA = TEST_OFP_DATA_JSON as unknown as OfpData
 const TEST_ODDS_DATA = TEST_ODDS_DATA_JSON as unknown as OddsData
 
 (async () => {
   const games = mergeOfpAndOddsData(TEST_OFP_DATA, TEST_ODDS_DATA)
-  console.log(games)
+  const outcomes = generateOutcomes(games)
+
+  console.log(outcomes[0])
 })()
