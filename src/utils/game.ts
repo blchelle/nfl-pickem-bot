b6c92304c732ce9315167f30c3e63a84bd229be6
Brock Chelle <brocklchelle@gmail.com>
2022-09-12 11:36:49 -0600

    #10: Create a function to calculate point results for each game

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,8 +1,19 @@
-import { GameData } from "../@types/gameData";
+import { GameData, ResultPoints } from "../@types/gameData";
+import { GameRank } from "../@types/ofpData";
 
-export const avgGamePoints = (game: GameData, totalPoints: number) => {
-  const awayPoints = game[0].pointDist * game[0].winProb * totalPoints
-  const homePoints = game[1].pointDist * game[1].winProb * totalPoints
+export const calcAvgGamePoints = (game: GameData, totalPoints: number) => {
+  const awayPoints = game[0].pointDist * game[0].winProb * totalPoints;
+  const homePoints = game[1].pointDist * game[1].winProb * totalPoints;
 
-  return +(awayPoints + homePoints).toFixed(3)
+  return +(awayPoints + homePoints).toFixed(3);
+};
+
+export const calcNetResultPoints = (game: GameData, pick: 0 | 1, rank: GameRank, totalPoints: number): ResultPoints => {
+  const avgGamePoints = calcAvgGamePoints(game, totalPoints);
+
+  const win = rank - avgGamePoints;
+  const lose = -avgGamePoints;
+  const avg = win * game[pick].winProb + lose * game[1 - pick].winProb;
+
+  return { avg, lose, win };
 };
